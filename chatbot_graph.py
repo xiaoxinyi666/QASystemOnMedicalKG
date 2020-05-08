a617088072f3dbd4f7d73e72aee460982586194e
xinyi <xiaoxinyi666@outlook.com>
2020-05-08 12:48:24 +0800

    Update chatbot_graph.py

--- chatbot_graph.py
+++ chatbot_graph.py
@@ -16,7 +16,7 @@ class ChatBotGraph:
         self.searcher = AnswerSearcher()
 
     def chat_main(self, sent):
-        answer = '您好，我是小勇医药智能助理，希望可以帮到您。如果没答上来，可联系https://liuhuanyong.github.io/。祝您身体棒棒！'
+        answer = '您好，我是新异健康助手，未能找到合适的答案，换个问题试一下吧'
         res_classify = self.classifier.classify(sent)
         if not res_classify:
             return answer
